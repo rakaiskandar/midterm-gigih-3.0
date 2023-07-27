1edb1b708ef3741f1d4681d4f8bd76604840d076
rakaiskandar <iskandarraka4@gmail.com>
2023-07-28 00:21:59 +0700

    feat: connect db with app

---
+++ src/databases/index.database.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const mongoString = process.env.DB_URL;
+
+const mongodb_connect = () => {
+    mongoose.connect(mongoString, {
+        useUnifiedTopology: true,
+		useNewUrlParser: true,
+    });
+
+    mongoose.connection.on("connected", () => {
+		console.log("Connected to database successfully");
+	});
+
+	mongoose.connection.on("error", (err) => {
+		console.log("Error while connecting to database :" + err);
+	});
+
+	mongoose.connection.on("disconnected", () => {
+		console.log("Mongodb connection disconnected");
+	});
+
+} 
+
+module.exports = mongodb_connect;
+

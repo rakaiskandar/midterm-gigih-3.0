58617186512075c218230fc1f71824ea118f93dc
rakaiskandar <iskandarraka4@gmail.com>
2023-07-28 10:59:23 +0700

    feat: get all products

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -2,6 +2,26 @@ const Product = require('../databases/models/product.model');
 const { api_response } = require('../utils/response.util');
 
 const get_product = async (req, res) => {
+    try {
+        const product = await Product.find();
+
+        if (!product.length) {
+            return api_response(404, req, res, {
+                error: "Products doesn't exist"
+            })
+        }
+
+        return api_response(200, req, res, {
+            products: product
+        });
+    } catch (e) {
+        return api_response(500, req, res, {
+            error: "Internal Server Error"
+        })
+    }
+}
+
+const get_product_id = async (req, res) => {
     const id = req.params.id;
 
     try {
@@ -20,11 +40,11 @@ const get_product = async (req, res) => {
         });
     } catch (e) {
         return api_response(500, req, res ,{
-            error: e.message
+            error: "Internal Server Error"
         })
     }
 }
 
 module.exports = {
-    get_product
+    get_product, get_product_id
 }
--- src/routes/api/product.route.js
+++ src/routes/api/product.route.js
@@ -1,12 +1,16 @@
 const { api } = require('../../configs/prefix.config');
-const { get_product } = require('../../controllers/product.controller');
+const { get_product, get_product_id } = require('../../controllers/product.controller');
 
 const express = require('express');
 
 const router = express.Router();
 
+router.get(`${api}/products`, (req, res) => {
+    get_product(req, res)
+})
+
 router.get(`${api}/products/:id`, (req, res) => {
-    get_product(req, res);
+    get_product_id(req, res);
 })
 
 module.exports = router;
